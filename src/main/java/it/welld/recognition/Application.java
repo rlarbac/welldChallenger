2253d3b74f5252d13bc1f502880535fe4e5b7f5e
Rodrigo de Azevedo <rlarbac@gmail.com>
2020-09-12 11:09:18 +0100

    Commented a line that indicated which classes do the controller and service rule bacause it is not necessary in my project.

--- src/main/java/it/welld/recognition/Application.java
+++ src/main/java/it/welld/recognition/Application.java
@@ -2,10 +2,9 @@ package it.welld.recognition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"it.weelid.recognition.service", "it.weelid.recognition.controller"})
+//@ComponentScan({"it.weelid.recognition.service", "it.weelid.recognition.controller"})
 public class Application {
 
 	public static void main(String[] args) {
